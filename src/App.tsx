ed27c92e31164568fbd838520ef3fb5baa6001ad
abhay <abhayforwork02@gmail.com>
2024-10-22 00:46:27 +0530

    added components

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Profile from "./components/Profile";
+import Sidebar from "./components/Sidebar";
+import Tabs from "./components/Tabs";
+
+const App = () => {
+  return (
+    <div>
+      <Sidebar />
+      <Profile />
+      <Tabs />
+    </div>
+  );
+};
+
+export default App;
