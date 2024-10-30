26e5a23e7c0e3f1dcdcef195bdfb7182b3b4e0b6
abhay <abhayforwork02@gmail.com>
2024-10-30 18:04:19 +0530

    components

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,9 @@
-import React from "react";
-
-const Card: React.FC = ({ title, description, image, link }) => {
+interface CardProps {
+  title: string;
+  description: string;
+  image: string;
+}
+const Card = ({ title, description, image }: CardProps) => {
   return (
     <div className="max-w-sm mx-auto m-[1rem] bg-[#0F0F0F] rounded-lg shadow-md overflow-hidden">
       <img src={image} alt={title} className="w-full h-48 object-cover" />
@@ -9,7 +12,7 @@ const Card: React.FC = ({ title, description, image, link }) => {
         <h2 className="text-2xl font-bold mb-2">{title}</h2>
         <p className="text-gray-400 mb-4">{description}</p>
         <a
-          href={link}
+          href="https://dl91era.com/"
           className="inline-block px-4 py-2 bg-white text-black font-semibold rounded-lg shadow hover:bg-gray-600 hover:text-white transition duration-200"
         >
           Learn More
